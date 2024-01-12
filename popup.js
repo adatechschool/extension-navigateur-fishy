50d9a0627d7fa020679fddd59dbd12a521d70600
Marion Ochem <marion.ochem@gmail.com>
2024-01-12 10:04:32 +0100

    last changes - again

--- popup.js
+++ popup.js
@@ -1,12 +1,12 @@
-// document.getElementById("search").addEventListener("click", () => {
-//   document.getElementById("overlay").style.display = "block";
-//   document.getElementById("popup").style.display = "block";
-//   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-//     chrome.tabs.update(tabs[0].id, {
-//       url: "https://mail.google.com/mail/u/0/#inbox",
-//     });
-//   });
-// });
+document.getElementById("search").addEventListener("click", () => {
+  document.getElementById("overlay").style.display = "block";
+  document.getElementById("popup").style.display = "block";
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    chrome.tabs.update(tabs[0].id, {
+      url: "https://mail.google.com/mail/u/0/#inbox",
+    });
+  });
+});
 
 // Demande au background.js de lui envoyer PTP
 document.getElementById("search").addEventListener("click", () => {
