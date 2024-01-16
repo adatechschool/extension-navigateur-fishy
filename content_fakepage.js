8c65f282407c9f427ff193eb5d6e491be0172b82
PDeslousPaoli <philippe.deslous-paoli@laposte.net>
2024-01-16 10:32:15 +0100

    re

--- content_fakepage.js
+++ content_fakepage.js
@@ -1,20 +1,31 @@
-// background need to send me the data object with title, img and price.
+// // background need to send me the data object with title, img and price.
 
-let thisTitle = document.getElementsByClassName("titre");
-let thisImage = document.querySelector("img.couverture");
-let thisPrice = document.querySelector(`[itemprop="offers"]`);
-console.log("title :", thisTitle.innerText);
-console.log("image :", thisImage.src);
-console.log("price :", thisPrice.innerText);
+// let thisTitle = document.getElementsByClassName("titre");
+// let thisImage = document.querySelector("img.couverture");
+// let thisPrice = document.querySelector(`[itemprop="offers"]`);
+// console.log("title :", thisTitle.innerText);
+// console.log("image :", thisImage.src);
+// console.log("price :", thisPrice.innerText);
 
-//this works only if the fake page information is organize as in amazon.com
-// thisTitle.innerText = thisTitle[0].innerText;
-// thisImage.src = thisImage.src;
-// thisPrice.innerHTML = thisPrice.innerText;
+// //this works only if the fake page information is organize as in amazon.com
+// // thisTitle.innerText = thisTitle[0].innerText;
+// // thisImage.src = thisImage.src;
+// // thisPrice.innerHTML = thisPrice.innerText;
 
-thisTitle.innerText = "Hello World";
-thisImage.src =
-  "https://m.media-amazon.com/images/I/31lL4h9km4L._SY445_SX342_.jpg";
-// thisTitle.innerText = thisTitle[0].innerText;
-// thisImage.src = thisImage.src;
-// thisPrice.innerHTML = thisPrice.innerText;
+// thisTitle.innerText = "Hello World";
+// thisImage.src =
+//   "https://m.media-amazon.com/images/I/31lL4h9km4L._SY445_SX342_.jpg";
+// // thisTitle.innerText = thisTitle[0].innerText;
+// // thisImage.src = thisImage.src;
+// // thisPrice.innerHTML = thisPrice.innerText;
+
+// const cart = () => {
+//   alert("you clicked on the button class");
+//   console;log("tralala")
+// };
+
+// const btn = document.getElementById("header-cart-link").addEventListener("click", cart);
+// const btn = document.getElementsByClassName("btn btn-brand-2 btAddToCart").addEventListener("click", cart);
+
+
+
